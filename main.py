0c623cf1d82a89fc0ffbc4f3748fd1cf92ed97e5
neekolad <74865802+neekolad@users.noreply.github.com>
2022-09-17 12:23:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,44 @@
+# Snail needs to get out of the well that is 3m deep. In every 10 minutes he advances 30cm, but then he has to take a break 1 minute, during that time he goes 20cm back. How much time will pass until the snail gets out of the well.
+
+from datetime import timedelta
+
+# Setting variables
+time = 0
+distance = 0
+distance_gain = 30
+rest_distance_drop = 20
+is_end = False
+
+# Main logic
+while is_end == False:
+
+  # Move up
+  distance+=distance_gain
+  # Add time
+  time+=10
+
+  # Print iteration results
+  print("Time is: ", time)
+  print("Distance is: ", distance)
+  print("-------------------")
+
+  # Check if we hit 3 meters
+  if distance >= 300:
+    is_end = True
+    print("OUT OF THE WELL!")
+  else:
+
+    # Resting for 1 minute
+    time+=1
+
+    # Deduct distance we lost while resting
+    distance-=rest_distance_drop
+
+# Calculate the exact time the snail reached the top of the well
+
+difference = distance - 300
+exact_time = time - (10 - 10*difference/distance_gain)
+delta = timedelta(minutes=exact_time)
+
+# Printing the results
+print("The exact time that snail reached 3 meters and left the well is: ", str(delta).split(".", 2)[0])
